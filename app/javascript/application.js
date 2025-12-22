b58b92ef549fdfb1876e6c6ffa8e89877a4b088e
Nisar Ahmad <nisarawan9415@gmail.com>
2025-12-22 12:16:08 +0500

    updated

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+document.addEventListener("turbo:load", () => {
+  const btn = document.getElementById("menu-btn");
+  const menu = document.getElementById("mobile-menu");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
+  }
+});
